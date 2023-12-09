b4115439b2a0b5c88a4d6b6e7ed24836a0321436
Armoghan <Armoghan16@gmail.com>
2023-12-09 18:26:50 +0500

    Added Tailwind Css

---
+++ assets/css/postcss.config.js
@@ -0,0 +1,20 @@
+/* This code is exporting a configuration object for a JavaScript module. */
+
+const themeDir = __dirname + '/../../';
+
+module.exports = {
+    plugins: [
+        require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
+        require('autoprefixer')({
+            path: [themeDir]
+        }),
+        // require('@fullhuman/postcss-purgecss')({
+        //     content: [
+        //         `${themeDir}layouts/**/*.html`,
+        //         `${themeDir}content/**/*.md`,
+        //         // Add any other file paths that include your CSS classes
+        //     ],
+        //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        // }),
+    ]
+}
